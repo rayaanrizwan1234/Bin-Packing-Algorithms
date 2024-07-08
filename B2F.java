7f784c46b47ff1060db5aa8ce27ea9460d610b46
kirilvino <bulber037@gmail.com>
2024-07-08 11:12:45 +0100

    Preliminary functional version of B2F created

--- B2F.java
+++ B2F.java
@@ -18,6 +18,24 @@ class B2F{
                 }
             }
             if (j == numberOfBins){
+                // have concerns about this iteration swapping elements in multiple different bins but don't want to use a break as it needs to check for the largest combined size
+                for (j = 0; j < numberOfBins; j++){
+                    int size = bins.get(j).size();
+                    int smallestItem = bins.get(j).get(size - 1);
+                    for (int k = 0; k < n - i ; k++){
+                        for (int l = 0; l < n - i ; l++ ){
+                            int pair = item[i+k] + item[i+l];
+                            if (resCap[j] + smallestItem - pair >= 0 && pair > smallestItem){
+                                bins.get(j).set(size - 1, item[i+k]);
+                                bins.get(j).add(item[i+l]);
+                                n -= 1;
+                                item[i+k] = smallestItem;
+                                break;
+
+                            }
+                        }
+                    }
+                }
                 bins.add(new ArrayList<Integer>());
                 resCap[numberOfBins] = cap - item[i];
                 bins.get(numberOfBins).add(item[i]);
@@ -33,7 +51,8 @@ class B2F{
     }
 
     public static void main( String[] args ) {
-        Integer items[] = {4, 4, 4, 2};
+        // B2F works for these values, haven't tried any others yet
+        Integer items[] = {6, 3, 2, 2};
         int cap = 10;
         int n = items.length;
         System.out.print("Number of bins required in First Fit Decreasing: " + firstFitDecreasing(items, n, cap) + "\n");
