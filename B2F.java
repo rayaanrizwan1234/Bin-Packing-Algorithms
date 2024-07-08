f443e3479609948e66d26e9fe8e292554aaf204a
kirilvino <bulber037@gmail.com>
2024-07-08 12:38:51 +0100

    Improved B2F, still needs item array fixing

--- B2F.java
+++ B2F.java
@@ -9,6 +9,8 @@ class B2F{
         ArrayList<ArrayList<Integer>> bins = new ArrayList<ArrayList<Integer> >();
         resCap[0] = cap;
         for (int i = 0; i < n; i++){
+            // new issue is that need to figure out how to skip past element if it has already been swapped in.
+            System.out.print(item[i] + "\n");
             int j;
             for (j = 0; j < numberOfBins; j++){
                 if (resCap[j] >= item[i]){
@@ -18,29 +20,41 @@ class B2F{
                 }
             }
             if (j == numberOfBins){
-                // have concerns about this iteration swapping elements in multiple different bins but don't want to use a break as it needs to check for the largest combined size
+                // this holds : bin where swap occured, smallest item in that bin, k value, l value, largest feasible pair found, the size of that bin
+                int []replacer = { 0, 0, 0, 0, 0, 0 };
                 for (j = 0; j < numberOfBins; j++){
                     int size = bins.get(j).size();
-                    int smallestItem = bins.get(j).get(size - 1);
-                    for (int k = 0; k < n - i ; k++){
-                        for (int l = 0; l < n - i ; l++ ){
-                            int pair = item[i+k] + item[i+l];
+                    int smallestItem = (bins.get(j).get(size - 1));
+                    for (int k = i; k < n; k++){
+                        for (int l = i; l < n; l++ ){
+                            int pair = item[k] + item[l];
                             if (resCap[j] + smallestItem - pair >= 0 && pair > smallestItem){
-                                bins.get(j).set(size - 1, item[i+k]);
-                                bins.get(j).add(item[i+l]);
-                                n -= 1;
-                                item[i+k] = smallestItem;
-                                break;
+                                if (pair > replacer[4]){
+                                    replacer[0] = j;
+                                    replacer[1] = smallestItem;
+                                    replacer[2] = k;
+                                    replacer[3] = l; 
+                                    replacer[4] = pair;
+                                    replacer[5] = size;
+                                }
                             }
                         }
                     }
                 }
-                bins.add(new ArrayList<Integer>());
-                resCap[numberOfBins] = cap - item[i];
-                bins.get(numberOfBins).add(item[i]);
-                numberOfBins++;
+                if (replacer[4] != 0){
+                    bins.get(replacer[0]).set(replacer[5] - 1, item[replacer[2]]);
+                    bins.get(replacer[0]).add(item[replacer[3]]);
+                    item[i+1] = replacer[1];
+                }
+                else{
+                    bins.add(new ArrayList<Integer>());
+                    resCap[numberOfBins] = cap - item[i];
+                    bins.get(numberOfBins).add(item[i]);
+                    numberOfBins++;
+                }
             }
         }
+        System.out.print(bins.get(1).get(0));
         return numberOfBins;
     }
     
@@ -51,7 +65,7 @@ class B2F{
 
     public static void main( String[] args ) {
         // B2F works for these values, haven't tried any others yet
-        Integer items[] = {6, 3, 2, 2};
+        Integer items[] = {5, 4, 3, 2};
         int cap = 10;
         int n = items.length;
         System.out.print("Number of bins required in First Fit Decreasing: " + firstFitDecreasing(items, n, cap) + "\n");
