168dc4336262bfa35d4147b953a7b669cd0d99b2
kirilvino <bulber037@gmail.com>
2024-07-09 09:59:49 +0100

    File reading added

--- B2F.java
+++ B2F.java
@@ -1,6 +1,9 @@
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.io.File;
 
 class B2F{
     static int bestTwoFit(Integer item[], int n, int cap) {
@@ -22,13 +25,14 @@ class B2F{
             if (j == numberOfBins){
                 // this holds : bin where swap occured, smallest item in that bin, k value, l value, largest feasible pair found, the size of that bin
                 int []replacer = { 0, 0, 0, 0, 0, 0 };
+                // checks whether any feasible swaps are possible
                 for (j = 0; j < numberOfBins; j++){
                     int size = bins.get(j).size();
                     int smallestItem = (bins.get(j).get(size - 1));
                     for (int k = i; k < n; k++){
                         for (int l = i; l < n; l++ ){
                             int pair = item[k] + item[l];
-                            if (resCap[j] + smallestItem - pair >= 0 && pair > smallestItem && pair > (cap * (1/6)) && k != l){
+                            if (resCap[j] + smallestItem - pair >= 0 && pair > smallestItem && pair > (cap * (1/6)) && k != l && size > 1){
                                 if (pair > replacer[4]){
                                     replacer[0] = j;
                                     replacer[1] = smallestItem;
@@ -41,14 +45,14 @@ class B2F{
                         }
                     }
                 }
-                // updates item and bins with largest feasible pair found
+                // updates item and bins with largest feasible pair found if there has been one found
                 if (replacer[4] != 0){
                     // System.out.print("Replacing " + replacer[1] + " in " + replacer[0] + " with " + item[replacer[2]] + " and " + item[replacer[3]] + "\n");
+                    resCap[replacer[0]] = resCap[replacer[0]] + replacer[1] - replacer[4];
                     bins.get(replacer[0]).set(replacer[5] - 1, item[replacer[2]]);
                     bins.get(replacer[0]).add(item[replacer[3]]);
-                    item[replacer[2]] = 0;
-                    item[replacer[3]] = 0;
-                    item[i+1] = replacer[1];
+                    item[replacer[2]] = replacer[1];
+                    item[replacer[3]] = item[i];
                 }
                 else{
                     bins.add(new ArrayList<Integer>());
@@ -67,10 +71,26 @@ class B2F{
     }
 
     public static void main( String[] args ) {
-        // B2F tested for: {6,3,2,2},{5,4,3,2},{4,4,3,3},{7,3,3,3,3,2,1}
-        Integer items[] = {4, 4, 3, 3};
-        int cap = 10;
-        int n = items.length;
-        System.out.print("Number of bins required in First Fit Decreasing: " + bestTwoDecreasing(items, n, cap) + "\n");
+        try {
+            File binText = new File("binpack1.txt");
+            Scanner textReader = new Scanner(binText);
+            int problems = Integer.parseInt(textReader.nextLine());
+            for (int i = 0; i < problems; i++){
+                System.out.print("Problem:" + textReader.nextLine() + "\n");
+                String data = textReader.nextLine().trim();
+                int cap = Integer.parseInt(data.substring(0, 3));
+                int n = Integer.parseInt(data.substring(4,7));
+                Integer []item = new Integer[n];
+                for (int j = 0; j < n; j++){
+                    data = textReader.nextLine();
+                    item[j] = Integer.parseInt(data);
+                }
+                System.out.print("Number of bins required in Best Two Fit: " + bestTwoFitDecreasing(item, n, cap) + "\n");
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.out.print("An error occured.\n");
+            e.printStackTrace();
+        }
     }
 }
