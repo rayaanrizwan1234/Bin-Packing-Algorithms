aee24efeb9503145f706c9dad593c5ee7ad4f22d
Rayaan Rizwan <sc21rr@leeds.ac.uk>
2024-07-10 14:49:28 +0100

    FFD two stage for item and bin trigger

--- Two-Stage/FFDTwoStage.java
+++ Two-Stage/FFDTwoStage.java
@@ -2,45 +2,40 @@ import java.util.Arrays;
 // import java.util.Collection;
 import java.util.ArrayList;
 import java.util.Collections;
-// import java.util.List;
-// import java.util.Scanner;
-// import java.io.FileNotFoundException; // Import this class to handle errors
-// import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException; // Import this class to handle errors
+import java.io.File;
 
 class FFDTwoStage {
-    static ArrayList<Integer> resCap;
-    static Integer[] firstFit(Integer items[], int cap, int numOfBins, int stage) {
-        resCap = new ArrayList<>(Collections.nCopies(numOfBins, cap));
-        final var n  = items.length;
-        for (int i = 0; i < n; i++) {
+    ArrayList<Integer> resCap = new ArrayList<>();
+    int numOfBins = 0;
+
+    Integer[] firstFit(Integer items[], int cap, int stage) {
+        for (int i = 0; i < items.length; i++) {
             int j;
             boolean allocated = false;
             for (j = 0; j < numOfBins; j++) {
                 if (resCap.get(j) >= items[i]) {
                     final var res = resCap.get(j) - items[i];
-                    resCap.set(j, res); 
+                    resCap.set(j, res);
                     allocated = true;
                     break;
                 }
             }
             if (stage == 1 && !allocated) {
-                System.out.println("item is "+items[i]);
                 return Arrays.copyOfRange(items, i, items.length);
             }
-            // if (stage == 2 && j == numberOfBins) {
-            //     resCap[numberOfBins] = cap - item[i];
-            //     numberOfBins++;
-            // }
+            if (stage == 2 && j == numOfBins && !allocated) {
+                resCap.add(cap - items[i]);
+                numOfBins++;
+            }
         }
         return new Integer[0];
     }
 
-    // static int firstFitDecreasing(Integer item[], int n, int capacity) {
-    //     Arrays.sort(item, Collections.reverseOrder());
-    //     return firstFit(item, n, capacity);
-    // }
+    boolean firstFitDecreasingBinOrItem(Integer items[], int capacity, double binOrItemRatio, boolean binTrigger) {
+        Arrays.sort(items, Collections.reverseOrder());
 
-    static boolean firstFitDecreasingBinOrItem(Integer items[], int capacity, double binRatio, boolean binTrigger) {
         double maximumBins = 0;
         if (binTrigger) {
             int sumOfItems = 0;
@@ -49,40 +44,52 @@ class FFDTwoStage {
             }
             maximumBins = (double) sumOfItems / capacity;
             maximumBins = Math.ceil(maximumBins);
-            maximumBins *= binRatio;
+            maximumBins *= binOrItemRatio;
             maximumBins = Math.ceil(maximumBins);
+            numOfBins = (int) maximumBins;
+
+            resCap.addAll(Collections.nCopies(numOfBins, capacity));
+
+            items = firstFit(items, capacity, 1);
+            firstFit(items, capacity, 2);
+        } else {
+            final var numItemsDouble1 = items.length * binOrItemRatio;
+            int numItems1 = (int) numItemsDouble1;
+
+            final var items1 = Arrays.copyOfRange(items, 0, numItems1);
+            final var items2 = Arrays.copyOfRange(items, numItems1, items.length);
+
+            firstFit(items1, capacity, 2);
+            firstFit(items2, capacity, 2);
         }
 
-        Arrays.sort(items, Collections.reverseOrder());
-        int stage  = 1;
-        System.out.println("max "+maximumBins);
-        items = firstFit(items, capacity, (int) maximumBins, stage);
-        // implement second stage here
         return true;
     }
 
-    // static int firstFitDecreasingCapacity(Integer item[], int n, int capacity, double m, double numOfItems, double binCapacity) {
-    //     Arrays.sort(item, Collections.reverseOrder());
-    //     return firstFit(item, n, capacity);
+    // static int firstFitDecreasingCapacity(Integer item[], int n, int capacity,
+    // double m, double numOfItems, double binCapacity) {
+    // Arrays.sort(item, Collections.reverseOrder());
+    // return firstFit(item, n, capacity);
     // }
 
     public static void main(String[] args) {
         // try {
-        //     File binText = new File("../Testing-Data/binpack1.txt");
+        //     File binText = new File("Testing-Data/binpack4.txt");
         //     try (Scanner textReader = new Scanner(binText)) {
         //         int problems = Integer.parseInt(textReader.nextLine());
         //         for (int i = 0; i < problems; i++) {
         //             System.out.print("Problem:" + textReader.nextLine() + "\n");
         //             String data = textReader.nextLine().trim();
         //             int capacity = Integer.parseInt(data.substring(0, 3));
-        //             int n = Integer.parseInt(data.substring(4, 7));
+        //             int n = Integer.parseInt(data.substring(4, 8));
         //             Integer[] item = new Integer[n];
         //             for (int j = 0; j < n; j++) {
         //                 data = textReader.nextLine();
         //                 item[j] = Integer.parseInt(data);
         //             }
-        //             final var res = firstFitDecreasingBinOrItem(item, capacity, 0.5, true);
-        //             System.out.print("Number of bins required in First Fit Decreasing: " + res + "\n");
+        //             FFDTwoStage ffdTwoStage = new FFDTwoStage();
+        //             ffdTwoStage.firstFitDecreasingBinOrItem(item, capacity, 0.5, false);
+        //             System.out.print("Number of bins required in First Fit Decreasing: " + ffdTwoStage.numOfBins+ "\n");
         //         }
         //     } catch (NumberFormatException e) {
         //         e.printStackTrace();
@@ -91,10 +98,10 @@ class FFDTwoStage {
         //     System.out.print("An error occured.\n");
         //     e.printStackTrace();
         // }
-        Integer[] items = new Integer[]{1, 2, 3, 4, 5, 6};
+        Integer[] items = new Integer[] { 1, 2, 3, 4, 5, 6, 2, 10 };
         int cap = 10;
-        final var res = firstFitDecreasingBinOrItem(items, cap, 0.5, true);
-        System.out.println("Number of bins required in First Fit Decreasing: " + res + "\n");
-
+        FFDTwoStage ffdTwoStage = new FFDTwoStage();
+        ffdTwoStage.firstFitDecreasingBinOrItem(items, cap, 0.5, false);
+        System.out.println("Number of bins required in First Fit Decreasing: " + ffdTwoStage.numOfBins + "\n");
     }
 }
