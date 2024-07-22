e6ddd2be40ccbc90d9e4d57feea05a436ce25ff9
Rayaan Rizwan <sc21rr@leeds.ac.uk>
2024-07-22 10:00:52 +0100

    Added MBWFD to BFD

--- Two-Stage/MBWFD_BFD/MbWfdToBfd_1.java
+++ Two-Stage/MBWFD_BFD/MbWfdToBfd_1.java
@@ -5,15 +5,16 @@ import java.util.Collections;
 import java.util.Scanner;
 import java.io.File;
 
-// MB(WFD) to FFD with item trigger
-public class MbWfdToFfd_2 {
+// MB(WFD) to FFD with Bin trigger
+public class MbWfdToBfd_1 {
     ArrayList<Integer> resCap = new ArrayList<>();
     int numOfBins = 0;
     int capacity;
 
     // Stage 1 MB(WFD) wiht bin limit
-    Boolean MbWfd(Integer[] items) {
+    Integer[] MbWfd(Integer[] items) {
         ArrayList<Integer> notAllocatedItems = new ArrayList<>();
+        
         for (int item : items) {
             int max = -1;
             int maxBin = -1;
@@ -31,72 +32,49 @@ public class MbWfdToFfd_2 {
             }
         }
 
-        return notAllocatedItems.size() > 0 ? false : true;
+        return notAllocatedItems.toArray(new Integer[0]);
     }
 
-    boolean firstFit(Integer items[]) {
+    boolean bestFitDecreasing(Integer items[]) {
         for (int item : items) {
-            int j;
-            boolean allocated = false;
-            for (j = 0; j < numOfBins; j++) {
-                if (resCap.get(j) >= item) {
-                    final var res = resCap.get(j) - item;
-                    resCap.set(j, res);
-                    allocated = true;
-                    break;
+            int min = capacity + 1;
+            int minBin = 0;
+            for (int j = 0; j < resCap.size(); j++) {
+                int remaining = resCap.get(j) - item;
+                if (resCap.get(j) >= item && (remaining < min)) {
+                    minBin = j;
+                    min = remaining;
                 }
             }
-            if (j == numOfBins && !allocated) {
+            if (min == capacity + 1) {
                 resCap.add(capacity - item);
                 numOfBins++;
+            } else {
+                resCap.set(minBin, min);
             }
         }
         return true;
     }
 
 
-    void hybridMbWfdToFfd(Integer[] items, int capacity, double itemRatio) {
+    void hybridMbWfdToFfd(Integer[] items, int capacity, double binRatio) {
         Arrays.sort(items, Collections.reverseOrder());
 
-        this.capacity = capacity;
-
-        int numItems1 = (int) Math.ceil(items.length * itemRatio);
-
-        final var items1 = Arrays.copyOfRange(items, 0, numItems1);
-        final var items2 = Arrays.copyOfRange(items, numItems1, items.length);
-
         int sumOfItems = 0;
-        for (Integer i : items1) {
+        for(int i : items) {
             sumOfItems += i;
         }
 
-        // run binary search over different m values
-        int lowerBound = (int) Math.ceil((double) sumOfItems / capacity);
-        int upperBound = items.length;
-        numOfBins = (int) lowerBound;
+        double maximumBins = (double) sumOfItems / capacity;
+        maximumBins = Math.ceil(maximumBins) * binRatio;
+        numOfBins = (int) Math.ceil(maximumBins);
 
-        ArrayList<Integer> tempResCap = new ArrayList<>();
-        int min = upperBound;
-        while (lowerBound <= upperBound) {
-            int middle = lowerBound + ((upperBound - lowerBound) /2);
-            this.numOfBins = middle;
-            resCap.clear();
-            resCap.addAll(Collections.nCopies(numOfBins, capacity));
-            final var success = MbWfd(items1);
+        resCap.addAll(Collections.nCopies(numOfBins, capacity));
 
-            if (!success) {
-                lowerBound = middle + 1;
-            } 
-             else {
-                tempResCap = new ArrayList<>(resCap);
-                min = middle;
-                upperBound = middle - 1;
-             }
-        }
-        resCap = tempResCap;
-        numOfBins = min;
+        this.capacity = capacity;
 
-        firstFit(items2);
+        items = MbWfd(items);
+        bestFitDecreasing(items);
     }
 
     public static void main(String[] args) {
@@ -116,8 +94,8 @@ public class MbWfdToFfd_2 {
                         item[j] = Integer.parseInt(data);
                     }
                     // Testing objects
-                    MbWfdToFfd_2 res = new MbWfdToFfd_2();
-                    res.hybridMbWfdToFfd(item, capacity, 0.450);
+                    MbWfdToBfd_1 res = new MbWfdToBfd_1();
+                    res.hybridMbWfdToFfd(item, capacity, 0.50);
                     System.out.println("Number of bins used "+ res.numOfBins);
                 }
             } catch (NumberFormatException e) {
@@ -129,7 +107,7 @@ public class MbWfdToFfd_2 {
         }
         // Integer[] items = new Integer[] {6, 6, 5, 5, 4, 3, 3, 2, 1, 1};
         // int capacity = 10;
-        // MbWfdToFfd_2 res = new MbWfdToFfd_2();
-        // res.hybridMbWfdToFfd(items, capacity, 0.67);
+        // MbWfdToBfd_1 res = new MbWfdToBfd_1();
+        // res.hybridMbWfdToFfd(items, capacity, 0.5);
     }   
 }
