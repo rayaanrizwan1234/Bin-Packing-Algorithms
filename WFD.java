27a3d1ded3855c9e53182797cb748aae5ee4cc1e
kirilvino <bulber037@gmail.com>
2024-08-01 12:03:12 +0100

    Example of more accurate timer

--- WFD.java
+++ WFD.java
@@ -34,22 +34,48 @@ public class WFD {
 
     public static void main(String[] args) {
         try {
-            File binText = new File("Testing-Data/binpack1.txt");
+            File binText = new File("Testing-Data/binpack4.txt");
             Scanner textReader = new Scanner(binText);
             int problems = Integer.parseInt(textReader.nextLine());
+            Integer[][] itemList = new Integer[problems][1000];
             for (int i = 0; i < problems; i++){
                 System.out.print("Problem:" + textReader.nextLine() + "\n");
                 String data = textReader.nextLine().trim();
                 int cap = Integer.parseInt(data.substring(0, 3));
-                int n = Integer.parseInt(data.substring(4,7));
+                int n = Integer.parseInt(data.substring(4,8));
                 Integer []item = new Integer[n];
                 for (int j = 0; j < n; j++){
                     data = textReader.nextLine();
-                    item[j] = Integer.parseInt(data);
+                    itemList[i][j] = Integer.parseInt(data);
                 }
-                System.out.print("Number of bins required in Best Fit Decreasing: " + worstFitDecreasing(item, n, cap) + "\n");
             }
+        
+        long startTime = System.nanoTime();
+        for (int i = 0; i < problems; i++) { 
+            System.out.print("Number of bins required in First Fit Decreasing: " + worstFitDecreasing(itemList[i], 1000, 150) + "\n");
         }
+        long endTime = System.nanoTime();
+        System.out.print("Time taken: " + (endTime - startTime) / 1000000  + "ms\n");
+        // File folder = new File("Testing-Data/Complexity Test");
+        //     File[] listOfFiles = folder.listFiles();
+        //     Arrays.sort(listOfFiles);
+        //     for (File file : listOfFiles) {
+        //         System.out.println(file.getName());   
+        //         try (Scanner textReader = new Scanner(file)) {
+        //             int n = Integer.parseInt(textReader.nextLine());
+        //             int cap = Integer.parseInt(textReader.nextLine());
+        //             Integer[] item = new Integer[n];
+        //             for (int j = 0; j < n; j++) {
+        //                 String data = textReader.nextLine();
+        //                 item[j] = Integer.parseInt(data);
+        //             }
+        //             long startTime = System.nanoTime();
+        //             worstFitDecreasing(item, n, cap);
+        //             long endTime = System.nanoTime();
+        //             System.out.println("Time taken: " + (endTime-startTime)/1000 + " microseconds.");
+        //         }
+        //     }
+    }
         catch (FileNotFoundException e) {
             System.out.print("An error occured.\n");
             e.printStackTrace();
