83ee941db37ce0714389af7e58f8442d9f726f40
Kiril Vinogradov <63188276+kirilvino@users.noreply.github.com>
2024-08-16 16:43:43 +0100

    Delete Trees directory

--- Trees/main.java
+++
@@ -1,9 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        TwoThreeTree twoThreeTree = new TwoThreeTree();
-        twoThreeTree.insert(10);
-        twoThreeTree.insert(5);
-        twoThreeTree.insert(5);
-        twoThreeTree.bfsList();
-    }
-}
